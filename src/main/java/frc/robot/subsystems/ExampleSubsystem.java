226beb867e19cebd2dadac1e91a0ee1745acbcdc
lonaslee <93890846+lonaslee@users.noreply.github.com>
2025-10-01 20:51:50 -0500

    add some example code

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -5,55 +5,61 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
+public class ExampleSubsystem extends SubsystemBase {
+    public SparkMax exampleMotor = new SparkMax(1, SparkLowLevel.MotorType.kBrushless);
 
-public class ExampleSubsystem extends SubsystemBase
-{
-    /** Creates a new ExampleSubsystem. */
-    public ExampleSubsystem() {}
-    
-    
+
     /**
-     * Example command factory method.
-     *
-     * @return a command
+     * Creates a new ExampleSubsystem.
      */
-    public Command exampleMethodCommand()
-    {
-        // Inline construction of command goes here.
-        // Subsystem::RunOnce implicitly requires `this` subsystem.
-        return runOnce(
-                () -> {
-                    /* one-time action goes here */
-                });
+    public ExampleSubsystem() {
+        exampleMotor.configure(new SparkMaxConfig()
+                        .smartCurrentLimit(50)
+                        .idleMode(SparkBaseConfig.IdleMode.kBrake),
+                SparkBase.ResetMode.kResetSafeParameters,
+                SparkBase.PersistMode.kPersistParameters);
     }
-    
-    
+
+    public Command startMotor() {
+        return runOnce(() -> {
+            exampleMotor.set(0.5);
+        });
+    }
+
+    public Command stopMotor() {
+        return runOnce(() -> {
+            exampleMotor.set(0);
+        });
+    }
+
     /**
      * An example method querying a boolean state of the subsystem (for example, a digital sensor).
      *
      * @return value of some boolean subsystem state, such as a digital sensor.
      */
-    public boolean exampleCondition()
-    {
+    public boolean exampleCondition() {
         // Query some boolean state, such as a digital sensor.
         return false;
     }
-    
-    
+
+
     @Override
-    public void periodic()
-    {
+    public void periodic() {
         // This method will be called once per scheduler run
     }
-    
-    
+
+
     @Override
-    public void simulationPeriodic()
-    {
+    public void simulationPeriodic() {
         // This method will be called once per scheduler run during simulation
     }
 }
